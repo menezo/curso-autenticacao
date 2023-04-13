db9bf7e9272193d46143e4ab6211e3aca4e88ccc
Menezo <joaovitormenezesm12@gmail.com>
2023-04-13 19:37:41 -0300

    Adding dependencies and connecting the APIs to PostgreSQL

---
+++ stateful/stateful-auth-api/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.user (id, username, password) VALUES (1, 'user.test', '$2a$10$dSGL0paUSaEPWRb2TEbAT.2hEqFRMPf6lQ29zYu0r5ppSyppY8nm6');
+INSERT INTO public.user (id, username, password) VALUES (2, 'user.new.test', '$2a$10$dSGL0paUSaEPWRb2TEbAT.2hEqFRMPf6lQ29zYu0r5ppSyppY8nm6');
